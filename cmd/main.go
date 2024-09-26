4f44d16aca49d779a128b5b678b4242b76bd0426
ThiccPan <ihilmiderian@gmail.com>
2024-09-27 00:10:54 +0700

    refactor: app and server config

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/thiccpan/library_information_system/internal/delivery/http"
+	"github.com/thiccpan/library_information_system/internal/config"
 )
 
 func main() {
-	server := echo.New()
-	server.GET("/healthcheck", func(c echo.Context) error { return c.JSON(200, map[string]any{"message": "ok!"}) })
-	routerConfig := http.EchoRouteConfig{
-		App: server,
-	}
-	routerConfig.SetupRoute()
-	server.Logger.Fatal(server.Start(":8080"))
+	app := echo.New()
+	db := config.SetupDB()
+	config.SetupApp(config.BootstrapConfig{
+		App: app,
+		DB:  db,
+	})
+	app.Logger.Fatal(app.Start(":8080"))
 }
