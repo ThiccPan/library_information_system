1e14c319a6adb35218dbaecf5503b29389dd5030
ThiccPan <ihilmiderian@gmail.com>
2024-09-27 10:33:44 +0700

    fix: update user entity to use serial num as id and add timestamp field

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,11 +1,15 @@
 package entity
 
+import "time"
+
 type User struct {
-	Id         string `gorm:"type:uuid;primaryKey"`
-	Email      string `gorm:"uniqueIndex;not null"`
-	Password   string `gorm:"not null"`
-	Name       string `gorm:"not null"`
-	Role_id    uint   `gorm:"not null"`
-	Role       Role   `gorm:"foreignKey:role_id;references:id"`
-	ProfileImg string ``
+	Id         uint      `gorm:"primaryKey"`
+	Email      string    `gorm:"uniqueIndex;not null"`
+	Password   string    `gorm:"not null"`
+	Name       string    `gorm:"not null"`
+	Role_id    uint      `gorm:"not null"`
+	Role       Role      `gorm:"foreignKey:role_id;references:id"`
+	ProfileImg string    ``
+	CreatedAt  time.Time ``
+	UpdatedAt  time.Time ``
 }
