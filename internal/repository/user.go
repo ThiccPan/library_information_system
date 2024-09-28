7d0e6386a8684f1b54d15f70360f9d07b5a02e3c
ThiccPan <ihilmiderian@gmail.com>
2024-09-28 16:01:11 +0700

    feat: user repository

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -24,22 +24,22 @@ func (u *UserRepository) GetAll(db *gorm.DB) ([]entity.User, error) {
 	return users, nil
 }
 
-func (u *UserRepository) GetByEmail(db *gorm.DB, user *entity.User) (error) {
+func (u *UserRepository) GetByEmail(db *gorm.DB, user *entity.User) error {
 	tx := db.Find(&user, "email = ?", user.Email)
 	return tx.Error
 }
 
-func (u *UserRepository) Add(db *gorm.DB, user *entity.User) (error) {
+func (u *UserRepository) Add(db *gorm.DB, user *entity.User) error {
 	tx := db.Create(user)
 	return tx.Error
 }
 
-func (u *UserRepository) UpdateById(db *gorm.DB, user *entity.User) (error) {
+func (u *UserRepository) UpdateById(db *gorm.DB, user *entity.User) error {
 	tx := db.Model(user).Where("id = ?", user.Id).Updates(user)
 	return tx.Error
 }
 
-func (u *UserRepository) Delete(db *gorm.DB, user *entity.User) (error) {
+func (u *UserRepository) Delete(db *gorm.DB, user *entity.User) error {
 	tx := db.Delete(user)
 	return tx.Error
 }
