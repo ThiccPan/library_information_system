37ad6e653608b191b7a362a46e41424129f9a13e
ThiccPan <ihilmiderian@gmail.com>
2024-10-01 10:13:33 +0700

    fix: table constrain stock > -1

--- internal/entity/book.go
+++ internal/entity/book.go
@@ -5,7 +5,7 @@ import "time"
 type Book struct {
 	Id        uint   `gorm:"primaryKey"`
 	Name      string `gorm:"type:varchar(100);index;not null"`
-	Stock     uint   `gorm:"not null;default:0"`
+	Stock     uint   `gorm:"not null;default:0;check:stock > -1"`
 	Author_id uint   `gorm:"not null"`
 	Author    Author `gorm:"foreignKey:author_id;references:id"`
 	Topic_id  uint   `gorm:"not null"`
